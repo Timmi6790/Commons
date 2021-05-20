657a8b428458926947be8fd2228de316da86cc6d
Tim <13711365+Timmi6790@users.noreply.github.com>
2021-05-20 08:56:00 +0200

    Add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module de.timmi.commons {
+    requires lombok;
+    requires com.google.gson;
+    requires org.checkerframework.checker.qual;
+
+    exports de.timmi6790.commons.builders;
+    exports de.timmi6790.commons.utilities;
+}
