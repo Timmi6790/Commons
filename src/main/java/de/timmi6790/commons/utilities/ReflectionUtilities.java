8071fcce7946aa9a0ec79a0eaaf8c1b15af5d36c
Tim <13711365+Timmi6790@users.noreply.github.com>
2021-05-20 08:56:30 +0200

    Remove deprecated ReflectionUtilities methods

--- src/main/java/de/timmi6790/commons/utilities/ReflectionUtilities.java
+++ src/main/java/de/timmi6790/commons/utilities/ReflectionUtilities.java
@@ -1,25 +1,20 @@
 package de.timmi6790.commons.utilities;
 
 import com.google.gson.Gson;
-import lombok.NonNull;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
-import lombok.experimental.UtilityClass;
 
-import java.io.File;
 import java.lang.annotation.Annotation;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.Optional;
 
 /**
  * Several reflection related utility functions.
  */
-@UtilityClass
-public class ReflectionUtilities {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ReflectionUtilities {
     /**
      * Creates a deep clone of the given object.
      *
@@ -28,7 +23,7 @@ public class ReflectionUtilities {
      * @return the deep cloned object
      */
     @SneakyThrows
-    public <O> O deepCopy(final O object) {
+    public static <O> O deepCopy(final O object) {
         // TODO: Implement a version based on reflections
         final Gson gson = new Gson();
         return gson.fromJson(gson.toJson(object), (Type) object.getClass());
@@ -43,45 +38,11 @@ public class ReflectionUtilities {
      * @param annotationClass the required annotation class
      * @return the found annotation
      */
-    public <T extends Annotation> Optional<T> getAnnotation(final Method method, final Class<T> annotationClass) {
+    public static <T extends Annotation> Optional<T> getAnnotation(final Method method, final Class<T> annotationClass) {
         try {
             final T annotation = method.getAnnotation(annotationClass);
             return Optional.of(annotation);
         } catch (final NullPointerException ignore) {
-            return Optional.empty();
-        }
-    }
-
-    /**
-     * Adds a jar file to the given classLoader.
-     *
-     * @param jar            the jar
-     * @param urlClassLoader the url class loader
-     * @throws MalformedURLException     the malformed url exception
-     * @throws InvocationTargetException the invocation target exception
-     * @throws IllegalAccessException    the illegal access exception
-     * @throws NoSuchMethodException     the no such method exception
-     */
-    public void addJarToClassLoader(final File jar, final URLClassLoader urlClassLoader)
-            throws MalformedURLException, InvocationTargetException, IllegalAccessException, NoSuchMethodException {
-        final Method method = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
-        method.setAccessible(true);
-        method.invoke(urlClassLoader, jar.toURI().toURL());
-    }
-
-    /**
-     * Tries to load the given class path from the given urlClassLoader. Instead of throwing a ClassNotFoundException,
-     * returns Optional::emtpy
-     *
-     * @param path           the path
-     * @param urlClassLoader the url class loader
-     * @return the optional
-     */
-    public Optional<Class<?>> loadClassFromClassLoader(@NonNull final String path,
-                                                       @NonNull final URLClassLoader urlClassLoader) {
-        try {
-            return Optional.of(urlClassLoader.loadClass(path));
-        } catch (final ClassNotFoundException ignore) {
             return Optional.empty();
         }
     }
